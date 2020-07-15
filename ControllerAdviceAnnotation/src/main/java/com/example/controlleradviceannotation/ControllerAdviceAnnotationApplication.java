5343f64213c7334a48f299ad7e0661cf92552e64
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-07-15 14:36:43 +0700

    Init Controller Advice module
    Init Controller Advice module

---
+++ ControllerAdviceAnnotation/src/main/java/com/example/controlleradviceannotation/ControllerAdviceAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlleradviceannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerAdviceAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerAdviceAnnotationApplication.class, args);
+	}
+
+}
---
+++ ControllerAdviceAnnotation/src/test/java/com/example/controlleradviceannotation/ControllerAdviceAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controlleradviceannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerAdviceAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
