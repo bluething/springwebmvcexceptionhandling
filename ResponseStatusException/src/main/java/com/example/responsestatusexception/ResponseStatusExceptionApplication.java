8d3c00332ca1d7dfc275d53e471f80edcdc1b073
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-07-15 14:37:24 +0700

    Init Response Status Exception module
    Init Response Status Exception module

---
+++ ResponseStatusException/src/main/java/com/example/responsestatusexception/ResponseStatusExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.responsestatusexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseStatusExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponseStatusExceptionApplication.class, args);
+	}
+
+}
---
+++ ResponseStatusException/src/test/java/com/example/responsestatusexception/ResponseStatusExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.responsestatusexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponseStatusExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
