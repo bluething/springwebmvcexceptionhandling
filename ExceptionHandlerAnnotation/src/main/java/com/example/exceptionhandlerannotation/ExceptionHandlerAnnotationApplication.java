f51771a53ac677a019097baa76e836e481e6bcf8
Habib Ali Machpud <habib.gunadarma@gmail.com>
2020-07-15 14:36:13 +0700

    Init Exception Handler module
    Init Exception Handler module

---
+++ ExceptionHandlerAnnotation/src/main/java/com/example/exceptionhandlerannotation/ExceptionHandlerAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptionhandlerannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlerAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandlerAnnotationApplication.class, args);
+	}
+
+}
